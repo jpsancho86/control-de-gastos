ef863ce287febc313612551512138a21623bcab1
jpsancho86 <jpsancho86@gmail.com>
2024-09-21 13:06:49 -0600

    added descrition input

--- inmersion-dev-aula2-proyecto-base/assets/js/script.js
+++ inmersion-dev-aula2-proyecto-base/assets/js/script.js
@@ -1,12 +1,17 @@
+//crear arrays para guardar datos
 let listaNombreGasto=[];
 let listaValorGasto=[];
+let listaDescripcionGasto=[];
 
 function clickBoton(){
+    //Obtener datos de entrada
     let nombreGasto= document.getElementById('nombreGasto').value;
     let valorGasto= document.getElementById('valorGasto').value;
-
+    let descripcionGasto= document.getElementById('descripcionGasto').value;
+    //Add valores
     listaNombreGasto.push(nombreGasto);
     listaValorGasto.push(valorGasto);
+    listaDescripcionGasto.push(descripcionGasto);
 
     if(Number(valorGasto)>150){
         alert("Este es un gasto de mas de $150");
@@ -16,14 +21,17 @@ function clickBoton(){
 }
 
 function actualizarLista(){
+    //Definir elementos a modificar en el DOM
     const listaElementos= document.getElementById('listaDeGastos');
     const total= document.getElementById('totalGastos');
+
     let htmlLista='';
     let totalGastos=0;
+
     listaNombreGasto.forEach((elemento,posicion)=>{
        const valorGasto=listaValorGasto[posicion];
-        
-       htmlLista+=`<li>${elemento} $${valorGasto}
+        let descripcion= listaDescripcionGasto[posicion];
+       htmlLista+=`<li>${elemento} ${descripcion} $${valorGasto}
                 <button onclick="eliminarBoton(${posicion});">Eliminar</button>             
                 </li>`;
 
@@ -39,12 +47,14 @@ function actualizarLista(){
 function limpiar(){
     document.getElementById('nombreGasto').value='';
     document.getElementById('valorGasto').value='';
+    document.getElementById('descripcionGasto').value='';
 }
 
 function eliminarBoton(posicion){
-    console.log(posicion);
+
     listaNombreGasto.splice(posicion,1);
     listaValorGasto.splice(posicion,1);
-
+    listaDescripcionGasto.splice(posicion,1);
+    
     actualizarLista();
 }
